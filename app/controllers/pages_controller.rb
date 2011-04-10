c0c188d687942684b1fa24e59a030b255500a9f8
Don Pottinger <don.pottinger@gmail.com>
2011-04-09 20:57:33 -0700

    Layout

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+
+  def index
+    @hello = "Hello World"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HopactApp::Application.config.secret_token = '864771a3168b9328ba010b402ed1bc7103047a7f6ecb250730f504dd63c164678f28808eb96379f68a94d4819855358acd2fa488543a846897120da928338935'
